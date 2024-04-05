61cb6f40dc1ec65f902102c5aea3eecfeabcb12e
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-04-04 23:25:50 -0500

    md:+ layout complete

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import { projects } from "@/constants";
 import Sample from "./sample";
 
 const Projects = () => {
   return (
-    <section className="projects">
+    <section className="projects" id="projects">
       <h1>Selected Works</h1>
-      <Sample />
+      <span className="flex 2xl:flex-row flex-col 2xl:justify-start items-center flex-wrap gap-5">
+        {projects.map((project, index) => (
+          <Sample
+            key={index}
+            title={project.title}
+            description={project.description}
+            image_url={project.image_url}
+            skills={project.skills}
+            live_link={project.live_link}
+            github_link={project.github_link}
+            redirect_link={project.redirect_link}
+          />
+        ))}
+      </span>
     </section>
   );
 };
