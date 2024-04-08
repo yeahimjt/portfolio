46de23ad56744a0362f2dbeabe92e6bb7b881283
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-04-08 04:55:43 -0500

    fixing html tag

--- components/waving.tsx
+++ components/waving.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const Spline = React.lazy(() => import("@splinetool/react-spline"));
 const Waving = () => {
   return (
-    <sectiontiotitotitontiotit className="w-[350px] h-[350px] rounded-full">
+    <section className="w-[350px] h-[350px] rounded-full">
       <Spline scene="https://prod.spline.design/xbdnmC9X1Umnq6NP/scene.splinecode" />
-    </sectiontiotitotitontiotit>
+    </section>
   );
 };
 
